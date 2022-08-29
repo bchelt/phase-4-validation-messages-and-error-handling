50737cc3f13c4844c6a5a6029dd953d8a06f3830
Brandon Helt <brandon.helt@yahoo.com>
2022-08-28 19:37:07 -0700

    Completed assignment

--- client/src/components/MovieForm.js
+++ client/src/components/MovieForm.js
@@ -13,6 +13,7 @@ function MovieForm() {
     discount: false,
     female_director: false,
   });
+  const [errors, setErrors] = useState([])
 
   function handleSubmit(e) {
     e.preventDefault();
@@ -23,8 +24,14 @@ function MovieForm() {
       },
       body: JSON.stringify(formData),
     })
-      .then((response) => response.json())
-      .then((newMovie) => console.log(newMovie));
+      .then((response) => { 
+        if (response.ok) {
+          response.json().then((newMovie) => console.log(newMovie));
+        } else {
+          response.json().then((errorData) => setErrors(errorData.errors))
+        }
+      })
+      //.then((newMovie) => console.log(newMovie));
   }
 
   function handleChange(e) {
@@ -38,6 +45,15 @@ function MovieForm() {
 
   return (
     <Wrapper>
+      {
+        errors.length > 0 && (
+          <ul style={{ color: "red" }}>
+            {errors.map((error) => (
+              <li key={error}>{error}</li>
+            ))}
+          </ul>
+        )
+      }
       <form onSubmit={handleSubmit}>
         <FormGroup>
           <label htmlFor="title">Title</label>
